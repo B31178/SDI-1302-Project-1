573e7b0c9f03d9f8d9d40e49b513409b12ac868f
B31178 <travisp@fullsail.edu>
2013-02-07 22:00:15 -0500

    First attempt.

--- pochintesta_travis_assignment1.js
+++ pochintesta_travis_assignment1.js
@@ -4,5 +4,26 @@
 //	My First Program
 
 //  My Variables
+var wantPizza = true; /*Boolean Conditional*/
+var numberToppings = 1; /*Number Conditional*/
+var pizzaYuck = "I don't like pizza"; /*String Conditional*/
 
+//  My Output
+console.log("Do you guys want pizza?");
 
+//	My Conditionals
+if (wantPizza === true) {
+	if (numberToppings === 1) {
+		console.log("Let's order a large pepperoni.");	
+	} else {
+		console.log("Just order a large cheese pizza then.");
+		}
+} else {
+	if (pizzaYuck === ("I don't like pizza.") {
+		console.log("Have a piece of celery.");
+		
+	} else {
+		pizzaYuck + "I may have a slice of plain cheese though.";
+		console.log(pizzaYuck);
+		}
+}
